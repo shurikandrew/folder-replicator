fa683a0981abf04e2518c49bd9e6180ca5decfd6
shurikandrew <s26865@pjwstk.edu.pl>
2025-07-19 11:52:55 +0200

    Addded IOOperations class for all read and write tasks. Implemented reading all necessary input data from user.

---
+++ FolderReplicator/IOOperations.cs
@@ -0,0 +1,77 @@
+namespace FolderReplicator;
+
+public static class IOOperations
+{
+    public static string GetFolderPathFromConsole()
+    {
+        var path = Console.ReadLine();
+
+        while (!Directory.Exists(path))
+        {
+            Console.Write("Sorry, the path is invalid or the program does not have access to it!\nProvide another path: ");
+            path = Console.ReadLine();
+        }
+
+        return path;
+    }
+    
+    public static string GetFolderPathFromConsole(string pathToCompare)
+    {
+        var path = Console.ReadLine();
+
+        while (!Directory.Exists(path) || path == pathToCompare)
+        {
+            Console.WriteLine(path == pathToCompare
+                ? "Sorry, the paths must be different!"
+                : "Sorry, the path is invalid or the program does not have access to it!");
+
+            Console.Write("Provide another path: ");
+            path = Console.ReadLine();
+        }
+
+        return path;
+    }
+    
+    public static string GetFilePathFromConsole(string sourcePath, string replicaPath)
+    {
+        var path = Console.ReadLine();
+
+        while (!File.Exists(path) || !Path.GetExtension(path).Equals(".log"))
+        {
+            if (File.Exists(path) && !Path.GetExtension(path).Equals(".log"))
+            {
+                Console.WriteLine("Sorry, the path must be to the file with .log extension!");
+            }
+            else if (path.Contains(sourcePath) || path.Contains(replicaPath))
+            {
+                Console.WriteLine("Sorry, log file must be outside of source and replica folders!");
+            }
+            else
+            {
+                Console.WriteLine("Sorry, the path is invalid or the program does not have access to it!");
+            }
+            
+            Console.Write("Provide another path: ");
+            path = Console.ReadLine();
+        }
+
+        return path;
+    }
+    public static int GetIntegerFromConsole()
+    {
+        var number = Console.ReadLine();
+        int res = -1;
+        bool flag;
+
+        while (!int.TryParse(number, out res) || res <= 0)
+        {
+            Console.WriteLine(int.TryParse(number, out res)
+                ? "Sorry, the number must be grater than 0!"
+                : "Sorry, you did not input a number!");
+            Console.Write("Provide another number: ");
+            number = Console.ReadLine();
+        }
+
+        return res;
+    }
+}
--- FolderReplicator/Program.cs
+++ FolderReplicator/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using FolderReplicator;
+
+Console.Write("Input path to the source folder: ");
+var sourcePath = IOOperations.GetFolderPathFromConsole();
+
+Console.Write("Input path to the replica folder: ");
+var replicaPath = IOOperations.GetFolderPathFromConsole(sourcePath);
+
+Console.Write("Input path to log file: ");
+var logPath = IOOperations.GetFilePathFromConsole(sourcePath, replicaPath);
+
+Console.Write("Input synchronization interval: ");
+var interval = IOOperations.GetIntegerFromConsole();
+
+
+while (true)
+{
+    string[] sourceFiles = Directory.GetFiles(sourcePath);
+    string[] replicaFiles = Directory.GetFiles(replicaPath);
+}
